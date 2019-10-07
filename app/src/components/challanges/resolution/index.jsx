f4e22b03281e016fb445e84f4ab0563c86b83005
Dante Nolasco <dnolascodante@gmail.com>
2019-10-07 08:55:24 -0300

    new problem component

--- app/src/components/challanges/resolution/index.jsx
+++ app/src/components/challanges/resolution/index.jsx
@@ -6,6 +6,7 @@ import Description from './description';
 import Action from './action';
 
 import Team from './interactievComponents/team';
+import Problem from './interactievComponents/problem';
 
 const Resolutuion = () => {
   const [index, setindex] = useState(0);
@@ -15,13 +16,17 @@ const Resolutuion = () => {
   };
 
   const back = () => {
-    setindex(index + 1);
+    setindex(index - 1);
   };
 
   return (
     <main>
       <Header />
-      <SwipeableViews disabled index={index}>
+      <SwipeableViews
+        containerStyle={{ height: 'calc(100vh - 72px)' }}
+        disabled
+        index={index}
+      >
         <>
           <Description
             videoUrl="https://www.youtube.com/embed/tH9Q_bYUjI0"
@@ -40,7 +45,7 @@ const Resolutuion = () => {
             number={2}
             fileUrl="https://app.projetomarvin.com/assets/pdf/Marvin+-+fase+01.pdf"
           />
-          <Action title="preenchendo o desafio" next={next}>
+          <Action title="preenchendo o desafio" next={next} back={back}>
             <Problem />
           </Action>
         </>
--- app/src/components/challanges/resolution/interactievComponents/problem.jsx
+++ app/src/components/challanges/resolution/interactievComponents/problem.jsx
@@ -1,12 +1,113 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Problem = ({next, back}) => {
+import TextField from '../../../_customComponents/textField';
+import TextArea from '../../../_customComponents/textArea';
+
+import classes from '../resolution.module.css';
+
+const labelStyle = {
+  marginBottom: 4,
+  fontWeight: 'normal',
+  textTransform: 'initial',
+};
+
+const inputStyle = {
+  width: '100%',
+};
+
+const Problem = ({ next, back }) => {
+  const [state, setState] = useState({
+    problem: '',
+    description: '',
+    causes: '',
+    public: '',
+    reports: '',
+    frequency: '',
+  });
+
+  const updateState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const save = () => {
+    next();
+  };
+
   return (
-    <div>
-      
+    <div className={classes.containter}>
+      <TextField
+        label="Defina o problema"
+        value={state.problem}
+        style={{ label: labelStyle, input: inputStyle }}
+        name="problem"
+        onChange={(e) => updateState(e)}
+      />
+      <br />
+      <TextArea
+        rows={3}
+        label="Descreva o problema definido"
+        value={state.description}
+        style={{ label: labelStyle, input: inputStyle }}
+        name="description"
+        onChange={(e) => updateState(e)}
+      />
+      <br />
+      <TextArea
+        rows={3}
+        label="Quais são as possíveis causas desse problema?"
+        value={state.causes}
+        style={{ label: labelStyle, input: inputStyle }}
+        name="causes"
+        onChange={(e) => updateState(e)}
+      />
+      <br />
+      <TextArea
+        rows={3}
+        label="Quem é o público afetado por esse problema?"
+        value={state.public}
+        style={{ label: labelStyle, input: inputStyle }}
+        name="public"
+        onChange={(e) => updateState(e)}
+      />
+      <br />
+      <TextArea
+        rows={3}
+        label="Em entrevista, qual é o relato do público afetado?"
+        value={state.reports}
+        style={{ label: labelStyle, input: inputStyle }}
+        name="reports"
+        onChange={(e) => updateState(e)}
+      />
+      <br />
+      <TextArea
+        rows={3}
+        label="Qual é a frequência que esse problema acontece?"
+        value={state.frequency}
+        style={{ label: labelStyle, input: inputStyle }}
+        name="frequency"
+        onChange={(e) => updateState(e)}
+      />
+      <div>
+        <button
+          // disabled={students.length < 2}
+          className={classes.next}
+          onClick={save}
+        >
+          avançar &gt;
+        </button>
+        <button
+          // disabled={students.length < 2}
+          className={classes.back}
+          onClick={back}
+        >
+          &lt; VOLTAR
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default Problem
+export default Problem;
