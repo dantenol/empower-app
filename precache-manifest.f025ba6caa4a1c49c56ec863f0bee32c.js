d8d42e06cb46e1e11e3b0d5f2341e6dc889a3a23
Dante Nolasco <dnolascodante@gmail.com>
2019-10-07 11:43:40 -0300

    Updates

---
+++ precache-manifest.f025ba6caa4a1c49c56ec863f0bee32c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40dda1248a99ca56fc71b6648fa84ed6",
+    "url": "/empower-app/index.html"
+  },
+  {
+    "revision": "6e5a203576afe418032d",
+    "url": "/empower-app/static/css/main.a6b315e7.chunk.css"
+  },
+  {
+    "revision": "2e7fc879dcc0e4641fe2",
+    "url": "/empower-app/static/js/2.99616111.chunk.js"
+  },
+  {
+    "revision": "6e5a203576afe418032d",
+    "url": "/empower-app/static/js/main.ac55f6f0.chunk.js"
+  },
+  {
+    "revision": "5e4ec3fd887ac65246fe",
+    "url": "/empower-app/static/js/runtime-main.9cffabb7.js"
+  },
+  {
+    "revision": "2207eeee7ca5a06dfb74bbde121dc788",
+    "url": "/empower-app/static/media/cloud-download.2207eeee.svg"
+  },
+  {
+    "revision": "8cdf168b46cb29d1f3c3201f7a67dc5a",
+    "url": "/empower-app/static/media/file.8cdf168b.svg"
+  },
+  {
+    "revision": "cdc0663aeeee09118ed4f5c665c7a5f0",
+    "url": "/empower-app/static/media/garbage.cdc0663a.svg"
+  }
+]);
