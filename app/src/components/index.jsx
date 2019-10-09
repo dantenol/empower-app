dccff5b1858aec528a4812b777ededc9901d6501
Dante Nolasco <dnolascodante@gmail.com>
2019-10-09 07:27:23 -0300

    Login view

--- app/src/components/index.jsx
+++ app/src/components/index.jsx
@@ -8,12 +8,14 @@ import {
 
 import Resolution from './challanges/resolution';
 import Login from "./userManagment/login";
+import Select from './challanges/select';
 
 const App = () => (
   <Router basename="/">
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/resolucao" component={Resolution} />
+      <Route path="/selecionar" component={Select} />
       <Route render={() => <Redirect to ="/login" />} />
     </Switch>
   </Router>
