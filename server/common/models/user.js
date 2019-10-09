79156b03fb04fcdcedf4bb8c0c79404f1194acba
Dante Nolasco <dnolascodante@gmail.com>
2019-10-09 12:07:27 -0300

    backend setup

--- server/common/models/user.js
+++ server/common/models/user.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(User) {
+  User.beforeRemote('create', async (ctx) => {
+    const body = ctx.req.body;
+    body.modifiedAt = new Date();
+    return;
+  });
 
+  User.beforeRemote('prorortpe.__create__challenge', async (ctx) => {
+    console.log(ctx.req);
+    const body = ctx.req.body;
+    body.modifiedAt = new Date();
+    body.modifiedBy = ctx.req.accessToken.userId;
+    return;
+  });
 };
