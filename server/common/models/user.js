4e42e8e7828671058255c535948adf3b48abade6
Dante Nolasco <dnolascodante@gmail.com>
2019-10-24 12:46:22 -0300

    fb login methods

--- server/common/models/user.js
+++ server/common/models/user.js
@@ -16,6 +16,11 @@ module.exports = function(User) {
     return;
   });
 
+  User.beforeRemote('login', (ctx) => {
+    const body = ctx.req.body;
+    body.ttl = 315360000;
+  });
+
   User.beforeRemote('prorortpe.__create__challenge', async (ctx) => {
     console.log(ctx.req);
     const body = ctx.req.body;
@@ -24,39 +29,52 @@ module.exports = function(User) {
     return;
   });
 
-  const uploadFileToS3 = (file) => {
-    const buffer = file.data;
-    return new Promise((resolve, reject) => {
-      return s3.upload(
-        {
-          Bucket: 'empower-uploads',
-          Key: file.name,
-          Body: buffer,
-        },
-        (err, result) => {
-          console.log(err, result);
-          if (err) reject(err);
-          else resolve(result);
-        },
-      );
-    });
+  User.fbLogin = async (data) => {
+    const AccessToken = User.app.models.AccessToken;
+    console.log(data);
+    const usr = await User.findOne({where: {email: data.email}});
+    console.log(usr);
+    if (data.status === 'unknown') {
+      throw "Algo deu errado no login";
+    }
+    if (!usr) {
+      const newUsr = await User.create({
+        name: data.name,
+        email: data.email,
+        fbId: data.id,
+        password: randomstring.generate(6),
+        modifiedAt: new Date(),
+      });
+      console.log(newUsr);
+      const newToken = {
+        ttl: 315360000,
+        created: new Date(),
+        userId: String(newUsr.id),
+      };
+      const token = await AccessToken.upsert(newToken, {upsert: false});
+      return {token, user: newUsr}
+    }
+    if (!usr.fbId) {
+      throw "Você já tem uma conta usando o email do Facebook: " + usr.email;
+    }
+    if (usr.fbId) {
+      if (data.id === usr.fbId) {
+        const newToken = {
+          ttl: 315360000,
+          created: new Date(),
+          userId: String(usr.id),
+        };
+        const token = await AccessToken.upsert(newToken, {upsert: false});
+        return {token, user: usr}
+      } else {
+        throw "Algo deu errado no login";
+      }
+    }
   };
 
-  User.upload = async (req) => {
-    if (!req.files) throw 'No files were uploaded.';
-    let file = Object.keys(req.files)[0];
-    const value = req.files[file];
-    file = (file, value);
-    const extension = /(?:\.([^.]+))?$/g.exec(file.name)[1];
-    file.name = randomstring.generate() + '.' + extension;
-    const { Key } = await uploadFileToS3(file);
-
-    return { result: 'success', id: Key };
-  };
-
-  User.remoteMethod('upload', {
-    accepts: [{ arg: 'req', type: 'object', http: { source: 'req' } }],
+  User.remoteMethod('fbLogin', {
+    accepts: [{ arg: 'data', type: 'object', http: { source: 'body' } }],
     returns: { root: true, type: 'object' },
-    http: { path: '/uploadFile', verb: 'post' },
+    http: { path: '/fbLogin', verb: 'post' },
   });
 };
